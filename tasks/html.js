5e0f59496672be5bc9fd6605f35e24219d6464c9
Rémy Hannequin <remy.hannequin@gmail.com>
2014-09-27 01:42:29 +0200

    Change expenses table and add moment.js

--- tasks/html.js
+++ tasks/html.js
@@ -1,13 +1,22 @@
 var gulp     = require('gulp')
   , jade     = require('gulp-jade')
   , plumber  = require('gulp-plumber')
+  , moment   = require('moment')
   , paths    = require('./paths')
   , utils    = require('../utils')
   , expenses = require('../' + paths.sources.root + 'data.json')
 
+moment.locale('fr')
+
 module.exports = function() {
   return gulp.src(paths.sources.root + 'index.jade')
     .pipe(plumber())
-    .pipe(jade({ pretty: true, locals: { expenses: utils.convert(expenses) } }))
+    .pipe(jade({
+        pretty: true
+      , locals: { 
+          expenses: utils.convert(expenses)
+        , moment: moment
+      }
+    }))
     .pipe(gulp.dest(paths.dist.root))
 }

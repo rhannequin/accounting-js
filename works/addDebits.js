2d3254200fbf17fa6e90f09e2ff7bb31fde2b35f
Rémy Hannequin <remy.hannequin@gmail.com>
2014-11-11 23:45:52 +0100

    Manage debits for one month

--- works/addDebits.js
+++ works/addDebits.js
@@ -1,5 +1,6 @@
 module.exports = function(expenses, debits) {
   var months = []
+
   expenses.forEach(function(expense) {
     var expenseDate = expense.date
     var month = new Date(expenseDate.getFullYear(), expenseDate.getMonth(), 1)
@@ -7,13 +8,24 @@ module.exports = function(expenses, debits) {
       months.push(month.getTime())
     }
   })
-  months = months.map(function(month) {
-    var d = new Date()
-    d.setTime(month)
-    return d
-  })
+
   debits.forEach(function(debit) {
     var date = new Date(debit.start_date)
+      , time = new Date(date.getFullYear(), date.getMonth(), 1).getTime()
+      , addToExpenses
+    if(months.indexOf(time) > -1) {
+      addToExpenses = {
+          label: debit.label
+        , means: debit.means
+        , date: date
+        , value: debit.value
+      }
+      if(typeof debit.categories !== 'undefined') {
+        addToExpenses.categories = debit.categories
+      }
+      expenses.push(addToExpenses)
+    }
   })
+
   return expenses
 }

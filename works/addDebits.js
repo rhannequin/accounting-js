59197db474c49756a3000d963f29c8f79b991fab
Rémy Hannequin <remy.hannequin@gmail.com>
2014-11-12 22:32:46 +0100

    Use moment.js

--- works/addDebits.js
+++ works/addDebits.js
@@ -1,19 +1,22 @@
+var moment = require('moment')
+
 module.exports = function(expenses, debits) {
   var months = []
 
   expenses.forEach(function(expense) {
     var expenseDate = expense.date
-    var month = new Date(expenseDate.getFullYear(), expenseDate.getMonth(), 1)
-    if(months.indexOf(month.getTime()) === -1) {
-      months.push(month.getTime())
+    var month = moment(expenseDate).format()
+    if(months.indexOf(month) === -1) {
+      months.push(month)
     }
   })
 
   debits.forEach(function(debit) {
     var date = new Date(debit.start_date)
-      , statTime = new Date(date.getFullYear(), date.getMonth(), 1).getTime()
+      , statTime = new moment(date).format()
       , addToExpenses
-    if(months.indexOf(statTime) > -1) {
+
+    if(months.indexOf(statTime) !== -1) {
       addToExpenses = {
           label: debit.label
         , means: debit.means

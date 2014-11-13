719a7c904945e83b0e156bfee1ac10769803cd1e
Rémy Hannequin <remy.hannequin@gmail.com>
2014-11-13 01:36:01 +0100

    Add debits for each months wanted

--- works/addDebits.js
+++ works/addDebits.js
@@ -3,32 +3,51 @@ var moment = require('moment')
 module.exports = function(expenses, debits) {
   var months = []
 
+  // Define all months involved
   expenses.forEach(function(expense) {
     var expenseDate = expense.date
-    var month = moment(expenseDate).format()
+    var month = moment(expenseDate).date(1).hour(23).format()
     if(months.indexOf(month) === -1) {
       months.push(month)
     }
   })
 
+  // For each debits, add it to each months
+  // from the date written, to the date written
   debits.forEach(function(debit) {
     var date = new Date(debit.start_date)
-      , statTime = new moment(date).format()
-      , addToExpenses
+      , startMoment = moment(date)
 
-    if(months.indexOf(statTime) !== -1) {
-      addToExpenses = {
+    do {
+      expenses.push(addToExpenses(debit, startMoment.toDate()))
+      startMoment.add(1, 'month')
+    } while(containsDate(months, startMoment))
+  })
+
+  function containsDate(months, date) {
+    var res = false
+    months.forEach(function(month) {
+      var m = moment(month).date(1).hour(23)
+      date.date(1).hour(23)
+      if(m.isSame(date)) {
+        res = true
+      }
+    })
+    return res
+  }
+
+  function addToExpenses(debit, date) {
+    var add = {
           label: debit.label
         , means: debit.means
-        , date: date
+        , date: new Date(date)
         , value: debit.value
       }
       if(typeof debit.categories !== 'undefined') {
-        addToExpenses.categories = debit.categories
+        add.categories = debit.categories
       }
-      expenses.push(addToExpenses)
-    }
-  })
+      return add
+  }
 
   return expenses
 }
